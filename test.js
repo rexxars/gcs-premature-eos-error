b0c21abfe7bf0c621d135f637a91cd570e28b4a7
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-10-24 22:04:24 +0200

    Working reproducible test case

---
+++ test.js
@@ -0,0 +1,19 @@
+require('hard-rejection/register')
+
+const fs = require('fs')
+const path = require('path')
+const brake = require('brake')
+const storage = require('@google-cloud/storage')
+
+const gcs = storage({
+  keyFilename: process.env.GCS_KEY_PATH || path.join(__dirname, 'gcloud.json')
+})
+
+const bucket = gcs.bucket(process.env.GCS_BUCKET || 'gcs-issue-test')
+const file = bucket.file(process.env.GCS_FILE_PATH || 'G0030540.JPG')
+const outputFile = path.join(__dirname, 'out.file')
+const targetStream = fs.createWriteStream(outputFile)
+const sourceStream = file.createReadStream()
+
+sourceStream.pipe(brake(10)).pipe(targetStream)
+sourceStream.once('readable', () => setImmediate(() => targetStream.end()))
